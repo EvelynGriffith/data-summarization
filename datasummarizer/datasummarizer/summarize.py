fc37873c8769db41808c8c6e646e8134e7ef1b7c
EvelynGriffith <griffith01@allegheny.edu>
2022-03-15 15:11:48 -0400

    adding to compute_mean function

--- datasummarizer/datasummarizer/summarize.py
+++ datasummarizer/datasummarizer/summarize.py
@@ -1,19 +1,27 @@
 # Add the required docstring to the module
 """Summarize the data through a compute mean function."""
 
-# TODO: add an import for all of the required type annotations
-
-import math
+# add an import for all of the required type annotations
+from typing import float
+from typing import List
 
 # Add a suitable docstring for the compute_mean function
 
-
 def compute_mean(numbers: List[float]) -> float:
     """Compute the mean for a list called numbers which contains floats."""
-    # TODO: sum the list of the numbers
-    # TODO: determine the length of the list of numbers
-    # TODO: as long as the computation will not be an
+    # sum the list of the numbers
+    Sum = sum(numbers)
+    # determine the length of the list of numbers
+    Size = len(numbers)
+    # as long as the computation will not be an
     # undefined division by zero, compute the mean
+    for i in numbers:
+        if i in numbers is not 0:
+            mean = Sum/Size
+            return mean
     # https://stackoverflow.com/questions/58400652/average-returning-a-value-even-when-list-is-empty
-    # TODO: if the list was empty, then return a mean that is "not a number"
+    # if the list was empty, then return a mean that is "not a number"
+        else:
+            if i is not None:
+                return "The mean is not a number."
     # https://stackoverflow.com/questions/944700/how-can-i-check-for-nan-values
--- datasummarizer/datasummarizer/transform.py
+++ datasummarizer/datasummarizer/transform.py
@@ -1,10 +1,10 @@
 # Add the required docstring to the module
 """Perform the transform_string_to_number_list function."""
+# Add an import for all of the required type annotations
 
-# TODO: Add an import for all of the required type annotations
+from typing import List 
 
 # Add a suitable docstring for the transform_string_to_number_list function
-
 
 def transform_string_to_number_list(data_text: str) -> List[float]:
     """Transform a string called data_text into a number to store into a list as a float."""
@@ -13,7 +13,7 @@ def transform_string_to_number_list(data_text: str) -> List[float]:
     # iterate through the data_text using the splitlines function
     for line in data_text.splitlines():
     # convert every one of the text-based values to a floating-point value
-        List = data_number_list.append(float(line))
+        new_list = data_number_list.append(float(line))
     # return the list of floating-point values that represent the
     # values that were converted from a string to a float
-    return List
+    return new_list
